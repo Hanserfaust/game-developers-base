0e66853c768d745cae87c6a21a35de236896345c
Hanserfaust <hans.eklund@igw.se>
2023-01-20 08:54:42 +0100

    Messages support kwargs now.

--- cocos2d_examples/example4/messages.py
+++ cocos2d_examples/example4/messages.py
@@ -5,7 +5,9 @@ class GameMessage(object):
     """
         Abstract base-class representing messages sent between client and server.
 
-        The data carried is message specific and are given as a tuple to the constructor.
+        The data carried is message specific is given to the constructor as args or
+        as kwargs and must match length of the given data_keys (no optional data).
+
     """
 
     # Below must be defined by subclasses
@@ -15,10 +17,16 @@ class GameMessage(object):
     # Set on server side upon deserialization
     client_id = None
 
-    def __init__(self, *data):
-        # Data is a tuple representing the keys in data_keys
-        assert len(data) == len(self.data_keys)
-        self.data = data
+    def __init__(self, *args, **kwargs):
+        if args:
+            assert len(args) == len(self.data_keys)
+            self.data = args
+        if kwargs:
+            assert len(kwargs.keys()) == len(self.data_keys)
+            data = list()
+            for key in self.data_keys:
+                data.append(kwargs[key])
+            self.data = tuple(data)
 
     def __str__(self):
         if self.client_id:
@@ -44,6 +52,10 @@ class GameMessage(object):
     def factory(bin_packet):
         """
         Given a binary packet, returns a ClientEvent-object of correct concrete class.
+
+        Used after receiving a bin_packet over the wire. Like so:
+
+        received_game_message = GameMessage.factory(bin_packet)
 
         :param bin_packet:
         :return:
