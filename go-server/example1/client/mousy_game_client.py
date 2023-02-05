3b4898a69631f150fe7d86de9d5e693fda6de0e4
Hanserfaust <hans.eklund@igw.se>
2023-02-05 10:28:05 +0100

    Refactor, protobuf generation as I want it now.

---
+++ go-server/example1/client/mousy_game_client.py
@@ -0,0 +1,81 @@
+import cocos
+import messages
+
+from tcp_client import MessageTCPClient
+
+
+# Subclass a Layer and define the logic of you program here:
+class MouseDisplay(cocos.layer.Layer):
+    is_event_handler = True  #: enable director.window events
+
+    def __init__(self):
+        super(MouseDisplay, self).__init__()
+
+        self.posx = 100
+        self.posy = 240
+        self.text = cocos.text.Label('No mouse events yet', font_size=18, x=self.posx, y=self.posy)
+        self.add(self.text)
+
+    def update_text(self, x, y):
+        text = 'Mouse @ %d,%d' % (x, y)
+        self.text.element.text = text
+        self.text.element.x = self.posx
+        self.text.element.y = self.posy
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """Called when the mouse moves over the app window with no button pressed
+
+        (x, y) are the physical coordinates of the mouse
+        (dx, dy) is the distance vector covered by the mouse pointer since the
+          last call.
+        """
+        self.update_text(x, y)
+
+        mouse_move_message = messages.MouseMove(x, y, left_click=False, right_click=False)
+        MessageTCPClient.send(bytes(mouse_move_message))
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        """Called when the mouse moves over the app window with some button(s) pressed
+
+        (x, y) are the physical coordinates of the mouse
+        (dx, dy) is the distance vector covered by the mouse pointer since the
+          last call.
+        'buttons' is a bitwise or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
+        'modifiers' is a bitwise or of pyglet.window.key modifier constants
+           (values like 'SHIFT', 'OPTION', 'ALT')
+        """
+        self.update_text(x, y)
+
+    def on_mouse_press(self, x, y, buttons, modifiers):
+        """This function is called when any mouse button is pressed
+
+        (x, y) are the physical coordinates of the mouse
+        'buttons' is a bitwise or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
+        'modifiers' is a bitwise or of pyglet.window.key modifier constants
+           (values like 'SHIFT', 'OPTION', 'ALT')
+        """
+        self.posx, self.posy = cocos.director.director.get_virtual_coordinates(x, y)
+        self.update_text(x, y)
+
+
+def main():
+
+    # Lets connect to server first to see that stuff works
+    gc = MessageTCPClient.get_instance(debug=True)
+    gc.connect()
+
+    gc.send(HelloServerGameMessage("Hello"))
+
+    # Init director
+    cocos.director.director.init()
+
+    mouse_display = MouseDisplay()
+
+    main_scene = cocos.scene.Scene(mouse_display)
+
+    # And let the director run the scene
+    cocos.director.director.run(main_scene)
+
+
+if __name__ == "__main__":
+    main()
---
+++ go-server/example1/client/tcp_client.py
@@ -0,0 +1,51 @@
+import random
+import socket
+
+
+class MessageTCPClient(object):
+    """
+        Singleton representing/abstracting interactions with the server.
+
+        Will _NOT_ connect upon creation.
+    """
+
+    _instance = None
+
+    # TODO: Supplied from server upon connect
+    id = random.randint(0, 2**31)
+
+    def __init__(self, hostname="127.0.0.1", port=7777, debug=False):
+        self.server = hostname
+        self.port = port
+        self.addr = (self.server, self.port)
+        self.debug = debug
+
+        # SOCK_STREAM means TCP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    @staticmethod
+    def get_instance(**kwargs):
+        if not MessageTCPClient._instance:
+            MessageTCPClient._instance = MessageTCPClient(**kwargs)
+        return MessageTCPClient._instance
+
+    def connect(self):
+        self.sock.connect(self.addr)
+
+    def _send(self, data):
+        try:
+            # is a high-level Python-only method that sends the entire buffer you pass or throws
+            # an exception. It does that by calling socket.send until everything has been sent
+            # or an error occurs.
+            self.sock.sendall(data)
+        except socket.error as e:
+            print(e)
+
+    @staticmethod
+    def send(game_message):
+        # Quick access method for sending data to server
+        gc = MessageTCPClient.get_instance()
+        if gc.debug:
+            print("Sending %s" % game_message)
+        bin_packet = game_message.serialize(MessageTCPClient.id)
+        gc._send(bin_packet)
